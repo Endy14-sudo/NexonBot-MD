f063d102764f94419c493a2393129c04e864e3bd
Endy14-sudo <u3778838036@gmail.com>
2026-03-10 07:12:44 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0",
-  updateZipUrl: "https://github.com/3ndyq1111-bot/Endy-bot/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/Endy14-sudo/NexonBot-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
